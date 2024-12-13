a077b54c6d7cc20795705f0d4057c7947c8ebe05
Luis <luis.ruizi@cirvianum.cat>
2024-12-13 19:31:35 +0100

    Video explicatiu

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ En aquest projecte
  - Gestionem reserves (CRUD)
  - Gestionem serveis (només admin)
  - entre altres...
+
+
+ <a href="https://drive.google.com/file/d/1dHaBw-sDtgdLd32GnkbciSMG16LwfUwD/view?usp=sharing">Enllaç al video explicatiu</a>
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,9 +12,7 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
+ 
     public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -22,9 +20,7 @@ class ProfileController extends Controller
         ]);
     }
 
-    /**
-     * Update the user's profile information.
-     */
+ 
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $user = $request->user(); // recuperem l'objecte amb les dades de l'usuari
@@ -40,9 +36,7 @@ class ProfileController extends Controller
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
-    /**
-     * Delete the user's account.
-     */
+  
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
